78f0b934b3d436ae4bab2982cbaecae563df3a21
tuncAksuna <aksuna.tunc@gmail.com>
2022-04-07 18:00:29 +0300

    builder pattern

---
+++ src/com/tuncode/patterns/dependencyinjection/builderpattern/Pizza.java
@@ -0,0 +1,90 @@
+package com.tuncode.patterns.dependencyinjection.builderpattern;
+
+public class Pizza {
+
+    private final int size; // final field - enter a value absolutely
+    private final String name; // enter a value absolutely
+    private boolean cheese;
+    private boolean olive;
+    private boolean pepperoni;
+    private boolean bacon;
+
+    /**
+     * private constructor - we ensured that this object is not called from outside
+     * */
+    private Pizza(PizzaBuilder pizzaBuilder) {
+        this.size = pizzaBuilder.size;
+        this.name = pizzaBuilder.name;
+        this.cheese = pizzaBuilder.cheese;
+        this.olive = pizzaBuilder.olive;
+        this.pepperoni = pizzaBuilder.pepperoni;
+        this.bacon = pizzaBuilder.bacon;
+    }
+
+    public static class PizzaBuilder {
+        private final int size;
+        private final String name;
+        private boolean cheese;
+        private boolean olive;
+        private boolean pepperoni;
+        private boolean bacon;
+
+        /**
+         *  constructor for "final fields" and a new object cannot be created without these two parameters
+         */
+        public PizzaBuilder(int size, String name) {
+            this.size = size;
+            this.name = name;
+        }
+
+        public PizzaBuilder withOptionalCheese(boolean cheese) {
+            this.cheese = cheese;
+            return this;
+        }
+
+        public PizzaBuilder withOptionalOlive(boolean olive) {
+            this.olive = olive;
+            return this;
+        }
+
+        public PizzaBuilder withOptionalPepperoni(boolean pepperoni) {
+            this.pepperoni = pepperoni;
+            return this;
+        }
+
+        public PizzaBuilder withOptionalBacon(boolean bacon) {
+            this.bacon = bacon;
+            return this;
+        }
+
+        public Pizza buildPizza() {
+            return new Pizza(this);
+        }
+    }
+
+    /**
+     * NE ZAMAN KULLANILIR ?
+     * 1) Eğer çok fazla parametre alan bir objemiz varsa bazı parametlerini mecburen "null" geçmek zorundaysak !
+     *  @ÖRNEK : new Pizza(20,true,true,null,null,null,null,true)
+
+     * 2) Farklı parametre kombinasyonlarına sahip "birden çok constructor" varsa ve hangilerini kullanacağımız konuları net değilse !
+
+     *  @ÖRNEK
+     *  public Pizza(int size)
+     *  public Pizza(int size,boolean cheese)
+     *  public Pizza(int size, boolean cheese, boolean pepperoni)
+     *  public Pizza(int size, boolean cheese, boolean pepperoni, boolean bacon)
+     *  gibi uzayıp giden bir constructor yapısı var !
+
+     * 3) Nesnenin oluşturulduktan sonra immutable(değişmez) olmasını istiyorsak !
+
+     * @ÖRNEK
+     * Pizza myPizza = new Pizza();
+     * myPizza.setSize(20);
+     * myPizza.setCheese(true);
+     * firinaGonder();
+     * myPizza.setSize(20);
+
+     * firinaGonder(); metodu çalıştıktan sonra bile setSize() metodu çalışabiliyor. Yani obje değiştiriliyor ve haberimiz bile olmuyor !
+     * */
+}
---
+++ src/com/tuncode/patterns/dependencyinjection/builderpattern/PizzaMain.java
@@ -0,0 +1,17 @@
+package com.tuncode.patterns.dependencyinjection.builderpattern;
+
+public class PizzaMain {
+    public static void main(String[] args) {
+
+        /**
+         * - The "size" or "name" parameters, which should not change after the object is created, cannot be changed internall
+         * - When creating the object, it is not created by passing unnecassary null parameters,
+         * */
+        Pizza.PizzaBuilder pizzaBuilder = new Pizza.PizzaBuilder(20, "Margharita");
+
+        pizzaBuilder.withOptionalCheese(true);
+        pizzaBuilder.withOptionalOlive(true);
+
+        Pizza myPizza = pizzaBuilder.buildPizza();
+    }
+}
