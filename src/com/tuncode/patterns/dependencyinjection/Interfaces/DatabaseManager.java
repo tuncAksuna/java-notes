441c12facf011e9560491dbbb59ef223410e972b
tuncAksuna <aksuna.tunc@gmail.com>
2022-04-21 16:33:32 +0300

    moved DatabaseManager class and added new comment line

---
+++ src/com/tuncode/patterns/dependencyinjection/Interfaces/DatabaseManager.java
@@ -0,0 +1,20 @@
+package com.tuncode.patterns.dependencyinjection.Interfaces;
+
+public class DatabaseManager {
+
+    public void add(Database database) {
+        database.add();
+    }
+
+    public void delete(Database database) {
+        database.delete();
+    }
+
+    public void update(Database database) {
+        database.update();
+    }
+
+    public void get(Database database) {
+        database.get();
+    }
+}
--- src/com/tuncode/patterns/dependencyinjection/Interfaces/Main.java
+++ src/com/tuncode/patterns/dependencyinjection/Interfaces/Main.java
@@ -1,27 +1,10 @@
 package com.tuncode.patterns.dependencyinjection.Interfaces;
 
-class DatabaseManager {
-    public void add(Database database) {
-        database.add();
-    }
-
-    public void delete(Database database) {
-        database.delete();
-    }
-
-    public void update(Database database) {
-        database.update();
-    }
-
-    public void get(Database database) {
-        database.get();
-    }
-}
-
 public class Main {
     /**
      * - Database classlarında bir değişiklik yaptığımız zaman bundan super classlar etkilenmeyecek !
-     * - Yeni bir database kullanımına geçileceği zaman yalnızca 'main' metodunda databaseManager referansı üzerinden yeni database'i fonksiyona parametre olarak vermemiz yeterli
+     * - Yeni bir database kullanımına geçileceği zaman yalnızca 'main' metodunda databaseManager referansı üzerinden yeni database'i fonksiyona parametre olarak vermemiz yeterli !
+     * - Artık daha kapsamlı ve ayno işleri yapan classlarımız var !
      */
     public static void main(String[] args) {
         DatabaseManager databaseManager = new DatabaseManager();
