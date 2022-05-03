23df337c949fb9944c1173b179dc2b98dab5227a
tuncAksuna <aksuna.tunc@gmail.com>
2022-05-03 13:43:46 +0300

    explained default methods in interfaces

--- src/com/tuncode/patternsandimportantopics/Interfaces/Database.java
+++ src/com/tuncode/patternsandimportantopics/Interfaces/Database.java
@@ -9,6 +9,12 @@ public interface Database {
 
     void get();
 
+    /*
+    Default metodlar ne için kullanılır ?
+    - Örneğin 10 class bu interface'yi implemente ediyor ve biz yalnızca bu 10 class arasından 6 classın bir metodu override etmesini istiyoruz
+    diğer 4 classın bu metodla bir işi olmayacak ! İşte bu gibi durumlarda default method tanımlaması implemente edilen classlar tarafından override hatası vermez
+    ihtiyaç duyulan sınıflarda override edilebilir.
+     */
     default String defaultMethodDefining() {
         return "Default method invoked and classes that implement this interface no longer have to override this method !";
     }
